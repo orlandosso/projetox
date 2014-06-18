2bd813130b797e27e95a373ea882533785b82034
orlandosso <orlandossouto@gmail.com>
2014-06-18 17:32:55 -0300

    Primeiro Commit Git Projeto X JSF 2.1 + JPA

---
+++ src/main/java/com/orlandosso/projetox/ProjetoXMB.java
@@ -0,0 +1,33 @@
+package com.orlandosso.projetox;
+
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+/**
+ * Projeto X
+ * @author SoutoOrS
+ *
+ */
+@ManagedBean
+@SessionScoped
+public class ProjetoXMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void enviar(){
+		
+	}
+}
---
+++ src/main/webapp/WEB-INF/lib/README.md
@@ -0,0 +1,4 @@
+projetox
+========
+
+Pasta de Bibliotecas
